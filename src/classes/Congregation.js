5a1af87bf8ccfc1530dcfcb40db372190a7a4d50
Steven Turner <steven.turner@rothco.com>
2020-04-15 20:01:37 -0400

    Creating Congregation Class

---
+++ src/classes/Congregation.js
@@ -0,0 +1,9 @@
+class Congregation {
+    constructor(props){
+        this.name = props.name;
+        this.congregation_id = props.congregation_id;
+        this.circuit = props.circuit;
+        this.address = props.address;
+        this.meetings = props.meetings;
+    }
+}
